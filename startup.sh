857f1d1aa952cf2af2636687748806e504b63f53
swanandingawale <swanand.ingawale@gmail.com>
2019-12-25 22:35:37 +0530

    Revert "Adding all three applications for the lab"
    This reverts commit a7a1d2bb1a2d8046e83df7b1aab87ff1176f2c56.

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#AppDynamics Envirnment variables
+APPD_OPTS="-javaagent:/sharedFiles/AppServerAgent/javaagent.jar"
+APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.applicationName=inventoryOsamaDocker"
+APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.tierName=inventory"
+APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.nodeName=docker-node"
+
+
+
+
+
+java  ${APPD_OPTS} -jar opentica-spring-boot-demo.jar
+#java -jar opentica-spring-boot-demo.jar
