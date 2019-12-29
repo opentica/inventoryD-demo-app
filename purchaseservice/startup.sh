8b5fbb21cf1cfc39d4e5d0e584d029bee8db7813
opentica <oawwad@opentica.com>
2019-12-28 20:27:21 -0500

    updated tier names for purchase and payment services

--- purchaseservice/startup.sh
+++ purchaseservice/startup.sh
@@ -2,7 +2,7 @@
 #AppDynamics Envirnment variables
 APPD_OPTS="-javaagent:/sharedFiles/AppServerAgent/javaagent.jar"
 APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.applicationName=inventoryOsamaDocker"
-APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.tierName=inventory"
+APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.tierName=purchase"
 APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.nodeName=docker-node"
 
 java  ${APPD_OPTS} -jar opentica-purchaseservice-demo.jar
