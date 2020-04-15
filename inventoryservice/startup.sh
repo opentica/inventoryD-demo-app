dab8f75dc2dba0dcfd36d0df7d057311478aaf9a
swanandingawale <swanand.ingawale@gmail.com>
2020-04-16 00:18:56 +0530

    Adding code changes for the elastic APM configuration

--- inventoryservice/startup.sh
+++ inventoryservice/startup.sh
@@ -5,9 +5,9 @@ APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.applicationName=inventoryOsamaDocker
 APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.tierName=inventory"
 APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.nodeName=docker-node"
 #IP address of the ELK APM server
-APPD_OPTS="${APPD_OPTS} -javaagent:/path/to/elastic-apm-agent-<version>.jar \
+APPD_OPTS="${APPD_OPTS} -javaagent:/sharedFiles/elastic-apm-agent-1.15.0.jar \
      -Delastic.apm.service_name=inventory-application \
-     -Delastic.apm.server_url=http://http://3.21.32.43/:8200 \
+     -Delastic.apm.server_url=http://3.21.32.43:8200 \
      -Delastic.apm.secret_token= \
      -Delastic.apm.application_packages=org.example"
 
