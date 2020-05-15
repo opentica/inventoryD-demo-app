01286c2107cac02129013d933a0514bd65d9d6d5
swanandingawale <swanand.ingawale@gmail.com>
2020-05-15 23:26:09 +0530

    Updaing the java launch options

--- inventoryservice/startup.sh
+++ inventoryservice/startup.sh
@@ -4,14 +4,14 @@ echo IS_APPD : ${IS_APPD}
 if [ "$IS_APPD" = 1 ]
 then
 	echo "Starting with AppDynamic configurartion settings"
-	APPD_OPTS="-javaagent:/sharedFiles/AppServerAgent/javaagent.jar"
-	APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.applicationName=inventoryOsamaDocker"
-	APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.tierName=inventory"
-	APPD_OPTS="${APPD_OPTS} -Dappdynamics.agent.nodeName=docker-node"	
+	JAVA_OPTS="-javaagent:/sharedFiles/AppServerAgent/javaagent.jar"
+	JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.applicationName=inventoryOsamaDocker"
+	JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.tierName=inventory"
+	JAVA_OPTS="${JAVA_OPTS} -Dappdynamics.agent.nodeName=docker-node"	
 else
 	#IP address of the ELK APM server
 	echo "Starting with ELK APM configurartion settings"
-	APPD_OPTS="${APPD_OPTS} -javaagent:/sharedFiles/elastic-apm-agent-1.15.0.jar \
+	JAVA_OPTS="${JAVA_OPTS} -javaagent:/sharedFiles/elastic-apm-agent-1.15.0.jar \
 		 -Delastic.apm.service_name=inventory-application \
 		 -Delastic.apm.server_url=http://3.21.32.43:8200 \
 		 -Delastic.apm.secret_token= \
