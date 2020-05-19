bd4f74422e63947d4c357dfac32ed72ac15d7093
swanandingawale <swanand.ingawale@gmail.com>
2020-05-19 22:22:47 +0530

    Adding code chanhges for marking the class as component

--- inventoryservice/src/main/java/com/opentica/inventory/app/util/ScheduledPurchaseTask.java
+++ inventoryservice/src/main/java/com/opentica/inventory/app/util/ScheduledPurchaseTask.java
@@ -9,10 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import com.opentica.inventory.app.bean.ProductPurchase;
 import com.opentica.inventory.app.service.ProductPurchaseService;
 
+@Component
 public class ScheduledPurchaseTask {
 	@Autowired
 	ProductPurchaseService purchaseService;
