927cb628dffdc315fbb336ff591d212784578e95
swanandingawale <swanand.ingawale@gmail.com>
2020-05-20 00:04:00 +0530

    Updating the scheduled cron job to call the inventory service endpoint instead of the purchase service endpoint

--- inventoryservice/src/main/java/com/opentica/inventory/app/util/ScheduledPurchaseTask.java
+++ inventoryservice/src/main/java/com/opentica/inventory/app/util/ScheduledPurchaseTask.java
@@ -1,5 +1,6 @@
 package com.opentica.inventory.app.util;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,10 +9,13 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import com.opentica.inventory.app.bean.ProductPurchase;
 import com.opentica.inventory.app.service.ProductPurchaseService;
@@ -20,10 +24,10 @@ import com.opentica.inventory.app.service.ProductPurchaseService;
 @EnableAsync
 @EnableScheduling
 public class ScheduledPurchaseTask {
-	@Autowired
-	ProductPurchaseService purchaseService;
+	@Value("${inventory.service.purchase.url}")
+	private String inventoryServicePurchaseUrl;
 
-	private static final Logger log = LoggerFactory.getLogger(ProductPurchaseService.class);
+	private static final Logger log = LoggerFactory.getLogger(ScheduledPurchaseTask.class);
 
 	private List<String> getProductList() {
 		List<String> list = new ArrayList<>();
@@ -33,8 +37,10 @@ public class ScheduledPurchaseTask {
 		list.add("Coffee Machine");
 		return list;
 	}
+
 	/**
 	 * Scheduled execution for the random product purchase scenario
+	 * 
 	 * @throws URISyntaxException
 	 */
 	// 30 seconds delay
@@ -48,7 +54,7 @@ public class ScheduledPurchaseTask {
 		List<String> productList = getProductList();
 		productPurchase.setProductName(productList.get(new Random().nextInt(productList.size())));
 		log.info("Starting the product purchase : " + productPurchase);
-		purchaseService.purchase(productPurchase);
+		this.purchase(productPurchase);
 	}
 
 	/**
@@ -63,11 +69,18 @@ public class ScheduledPurchaseTask {
 		ProductPurchase productPurchase = new ProductPurchase();
 		productPurchase.setCustomerId("C" + random.nextInt(999));
 		productPurchase.setPrice(random.nextInt(999));
-		//Z1 is failure code for the failure request
+		// Z1 is failure code for the failure request
 		productPurchase.setProductId("Z1");
 		List<String> productList = getProductList();
 		productPurchase.setProductName(productList.get(new Random().nextInt(productList.size())));
 		log.info("Starting the product purchase for Z1 : " + productPurchase);
-		purchaseService.purchase(productPurchase);
+		this.purchase(productPurchase);
+	}
+
+	private void purchase(ProductPurchase product) throws URISyntaxException {
+		RestTemplate restTemplate = new RestTemplate();
+		URI payUrl = new URI(inventoryServicePurchaseUrl);
+		ResponseEntity<Boolean> result = restTemplate.postForEntity(payUrl, product, Boolean.class);
+		log.info("Product purchase order is placed for product " + product)
 	}
 }
